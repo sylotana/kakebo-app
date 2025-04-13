6c53ab7823c3c660519a4ccd68d49703da7d6c7b
sylotana <anatolyshangin@mail.ru>
2025-04-13 08:36:04 +0300

    feat: adding menu and user selection

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 print("Budgetshell App for Tracking Expenses")
 
-income = input("Enter your day income: ")
-expense = input("Enter your day expense")
+while True:
+    # Menu
+    print(
+        "Menu",
+        "1. Add income",
+        "2. Add expense",
+        "3. Exit",
+        sep='\n'
+    )
+    menu_select = input()
+
+    if menu_select == '1':
+        income = input("Enter your day income: ")
+    elif menu_select == '2':
+        expense = input("Enter your day expense: ")
+    elif menu_select == '3':
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid input. Try again.")
 
 print("Your income:", income)
 print("Your expense:", expense)
