aec89edb2715a31171ee5770c32b20c480e2c8ab
sylotana <anatolyshangin@mail.ru>
2025-04-14 23:17:36 +0300

    feat: calculate the total amount of income and expenses, calculate the current balance

--- main.py
+++ main.py
@@ -11,21 +11,60 @@ menu_items = [
     "(3) Add expenses",
     "(4) Exit"
 ]
+
+total_income = 0.0
+total_expenses = 0.0
+
 while True:
     print("\nSelect a menu item:")
     print(*menu_items, sep="\n")
 
     choice = input("Select: ")
 
-    if menu_select == '1':
-        income = input("Enter your day income: ")
-    elif menu_select == '2':
-        expense = input("Enter your day expense: ")
-    elif menu_select == '3':
+    if choice == "1":
+        balance = total_income - total_expenses
+        print(
+            "=====================================",
+            f"Total balance: {balance}",
+            f"Total income: {total_income}",
+            f"Total expenses: {total_expenses}",
+            "=====================================",
+            sep='\n'
+        )
+        input(
+            "Press Enter to return to the menu."
+        )
+
+    elif choice == "2":
+        income = float(input("Enter your income: "))
+        total_income += income
+        print(
+            "=====================================",
+            "Income added successfully.",
+            f"Current income: {total_income} (+{income}).",
+            "=====================================",
+            sep="\n"
+        )
+        input(
+            "Press Enter to return to the menu."
+        )
+
+    elif choice == "3":
+        expenses = float(input("Enter your expenses: "))
+        total_expenses += expenses
+        print(
+            "=====================================",
+            "Expenses added successfully.",
+            f"Current expenses: {total_expenses} (-{expenses}).",
+            "=====================================",
+            sep="\n"
+        )
+        input(
+            "Press Enter to return to the menu."
+        )
+
+    elif choice == "4":
         print("Goodbye!")
         break
     else:
         print("Invalid input. Try again.")
-
-print("Your income:", income)
-print("Your expense:", expense)
