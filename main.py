b398fea0e7e15aee15ba0dddf51fbf685b3a097a
sylotana <anatolyshangin@mail.ru>
2025-04-16 22:31:29 +0300

    refactor: Change table display, add common divider function

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+def divider(count, symbol="-"):
+    return symbol * count
+
+
 print(
-    "-------------------------------------",
+    divider(40),
     "Budgetshell App for Tracking Expenses",
-    "-------------------------------------",
+    divider(40),
     sep="\n"
 )
 
@@ -25,20 +29,22 @@ while True:
 
     choice = input("Select: ")
 
+    # GET BALANCE
     if choice == "1":
         balance = round(total_incomes - total_expenses, 2)
         print(
-            "=====================================",
+            divider(40, "="),
             f"Total balance: {balance}",
             f"Total income: {total_incomes}",
             f"Total expenses: {total_expenses}",
-            "=====================================",
+            divider(40, "="),
             sep='\n'
         )
         input(
             "Press Enter to return to the menu."
         )
 
+    # ADD INCOME
     elif choice == "2":
         income_comment, income = (
             input('Enter your income in the format: <comment> <income>: ')
@@ -48,16 +54,17 @@ while True:
         total_incomes += float(income)
 
         print(
-            "=====================================",
+            divider(40, "="),
             "Income added successfully.",
             f"Current income: {total_incomes} (+{income}).",
-            "=====================================",
+            divider(40, "="),
             sep="\n"
         )
         input(
             "Press Enter to return to the menu."
         )
 
+    # ADD EXPENSE
     elif choice == "3":
         expense_comment, expense = (
             input('Enter your expense in the format: <comment> <expense>: ')
@@ -67,42 +74,45 @@ while True:
         total_expenses += float(expense)
 
         print(
-            "=====================================",
+            divider(40, "="),
             "Expenses added successfully.",
             f"Current expenses: {total_expenses} (+{expense}).",
-            "=====================================",
+            divider(40, "="),
             sep="\n"
         )
         input(
             "Press Enter to return to the menu."
         )
 
+    # GET INCOMES
     elif choice == "4":
         if incomes:
-            print("---------------------------------")
-            print(f"| TOTAL\t|\t{total_incomes}\t|")
-            print("---------------------------------")
+            print(divider(40))
+            print(f"| {"TOTAL".ljust(18)}|{str(total_incomes).rjust(17)} |")
+            print(divider(40))
             for comment, income in incomes:
-                print(f"| {comment}\t|\t{income}\t|")
-                print("---------------------------------")
+                print(f"| {comment.ljust(18)}|{str(income).rjust(17)} |")
+                print(divider(40))
         else:
             print("The income list is empty.")
 
         input("Press Enter to return to the menu.")
-    
+
+    # GET EXPENSES
     elif choice == "5":
         if expenses:
-            print("---------------------------------")
-            print(f"| TOTAL\t|\t{total_expenses}\t|")
-            print("---------------------------------")
+            print(divider(40))
+            print(f"| {"TOTAL".ljust(18)}|{str(total_expenses).rjust(17)} |")
+            print(divider(40))
             for comment, expense in expenses:
-                print(f"| {comment}\t|\t{expense}\t|")
-                print("---------------------------------")
+                print(f"| {comment.ljust(18)}|{str(expense).rjust(17)} |")
+                print(divider(40))
         else:
             print("The expenses list is empty.")
 
         input("Press Enter to return to the menu.")
 
+    # EXIT
     elif choice == "6":
         print("Goodbye!")
         break
