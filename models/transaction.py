bb60bf22e13d137ddb377d0991ccb15879f242fb
sylotana <anatolyshangin@mail.ru>
2025-05-05 23:34:44 +0300

    refactor: convert Transaction model to use @dataclass

--- models/transaction.py
+++ models/transaction.py
@@ -1,9 +1,9 @@
-class Transaction:
-    def __init__(self, date: str, t_type: str, amount: float, comment: str):
-        self.date = date
-        self.t_type = t_type
-        self.amount = amount
-        self.comment = comment
+from dataclasses import dataclass
 
-    def __str__(self):
-        return f"{self.date};{self.t_type};{self.amount};{self.comment}"
+
+@dataclass
+class Transaction:
+    date: str
+    t_type: str
+    amount: float
+    comment: str
