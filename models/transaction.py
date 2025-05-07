2da59414a153f0557f3a4d82d3f0ef5c3c7affc5
sylotana <anatolyshangin@mail.ru>
2025-05-07 23:14:48 +0300

    refactor: add unique ID to Transaction model

--- models/transaction.py
+++ models/transaction.py
@@ -1,8 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+import uuid
 
 
 @dataclass
 class Transaction:
+    id: str = field(default_factory=lambda: str(uuid.uuid4()))
     date: str
     t_type: str
     amount: float
