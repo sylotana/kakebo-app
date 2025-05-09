f4ac2f8179df7ec36b40aff9a9c05d68ecc0cc8f
sylotana <anatolyshangin@mail.ru>
2025-05-09 23:02:20 +0300

    fix: change the order of class properties

--- models/transaction.py
+++ models/transaction.py
@@ -4,8 +4,8 @@ import uuid
 
 @dataclass
 class Transaction:
-    id: str = field(default_factory=lambda: str(uuid.uuid4()))
     date: str
     t_type: str
     amount: float
     comment: str
+    id: str = field(default_factory=lambda: str(uuid.uuid4()))
